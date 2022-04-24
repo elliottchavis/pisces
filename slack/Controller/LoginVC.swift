46105f88a49d229b1781ccd51b7029a4733550be
johny babylon <ztredded@gmail.com>
2022-04-24 01:49:33 -0500

    loginVC UX/UI

---
+++ slack/Controller/LoginVC.swift
@@ -0,0 +1,122 @@
+//
+//  LoginVC.swift
+//  slack
+//
+//  Created by elliott chavis on 4/23/22.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    // MARK: - Properties
+    
+    private lazy var exitButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(#imageLiteral(resourceName: "multiply"), for: .normal)
+        //button.setImage(#imageLiteral(resourceName: "closeButton"), for: .normal)
+        button.tintColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        button.addTarget(self, action: #selector(dismissLoginVC), for: .touchUpInside)
+        return button
+    }()
+    
+    private let imageViewOne: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "slack_icon")
+        return iv
+    }()
+    
+    private lazy var emailContainerView: UIView = {
+        return InputContainerView(image: UIImage(systemName: "envelope"), textField: emailTextField)
+    }()
+    
+    private lazy var passwordContainerView: InputContainerView = {
+        let containerView = InputContainerView(image: UIImage(systemName: "lock"), textField: passwordTextField)
+        containerView.backgroundColor = .clear
+        
+        return containerView
+    }()
+    
+    private let emailTextField: UITextField = {
+        let txt = UITextField()
+        txt.placeholder = "Email"
+        txt.textColor = .darkGray
+        return txt
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let txt = UITextField()
+        txt.placeholder = "Password"
+        txt.isSecureTextEntry = true
+        txt.textColor = .darkGray
+
+        return txt
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setHeight(height: 50)
+        button.setTitleColor(.black, for: .normal)
+        button.isEnabled = false
+        //button.addTarget(self, action: #selector(handleLogin),for: .touchUpInside)
+        return button
+    }()
+    
+    private let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account? ", attributes: [.font: UIFont.systemFont(ofSize: 16), .foregroundColor: UIColor.gray])
+        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [.font: UIFont.boldSystemFont(ofSize: 16), .foregroundColor: UIColor.darkGray]))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        //button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.makeCorner(withRadius: 30)
+        configureUI()
+    }
+    
+    // MARK: - Actions
+    
+    @objc func dismissLoginVC() {
+        self.dismiss(animated: true, completion: nil)
+
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        
+        view.addSubview(exitButton)
+        exitButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 30, paddingRight: 30)
+        exitButton.setDimensions(height: 75, width: 75)
+        
+        view.addSubview(imageViewOne)
+        imageViewOne.anchor(top: exitButton.bottomAnchor, paddingTop: 30)
+        imageViewOne.centerX(inView: view)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView, loginButton])
+        stack.axis = .vertical
+        stack.spacing = 16
+        
+        view.addSubview(stack)
+        stack.anchor(top: imageViewOne.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor( left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor,paddingLeft: 32, paddingRight: 32)
+        
+        
+        //emailTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        //passwordTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+    }
+    
+
+}
--- slack/Utils/Extensions.swift
+++ slack/Utils/Extensions.swift
@@ -18,10 +18,26 @@ extension UIViewController {
         view.layer.addSublayer(gradient)
         gradient.frame = view.frame
     }
-    
+}
+
+extension UITextField {
+    func addBottomBorder(){
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 1)
+        bottomLine.backgroundColor = UIColor.gray.cgColor
+        borderStyle = .none
+        layer.addSublayer(bottomLine)
+    }
 }
 
 extension UIView {
+    
+    func makeCorner(withRadius radius: CGFloat) {
+            self.layer.cornerRadius = radius
+            self.layer.masksToBounds = true
+            self.layer.isOpaque = false
+        }
+    
     func anchor(top: NSLayoutYAxisAnchor? = nil,
                 left: NSLayoutXAxisAnchor? = nil,
                 bottom: NSLayoutYAxisAnchor? = nil,
